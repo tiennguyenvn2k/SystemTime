10fae34d8fbec5576f024d77acdd1a1bdf5196bb
tiennguyenvn2k <hiiamgamer2000@gmail.com>
2023-12-06 21:11:31 +0700

    da hoan thanh du an

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Systemtime;
+ class Program
+{
+    private static void Main(string[] args)
+    {
+        DateTime localDate = DateTime.Now;
+        System.Console.WriteLine("Datetime Now is :" + localDate);
+    }
+}
